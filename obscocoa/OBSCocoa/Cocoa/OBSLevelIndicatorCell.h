2a473c34790f83f78d663fec8ba514d6574af7f4
Fabio Russo <fabio@objectivesheep.com>
2011-12-02 22:50:54 +0100

    Indicator cell with dots

---
+++ obscocoa/OBSCocoa/Cocoa/OBSLevelIndicatorCell.h
@@ -0,0 +1,17 @@
+/* *****************************************************************
+ *                         ObjectiveSheep Framework        
+ * -----------------------------------------------------------------
+ *  Copyright (c) 2010 ObjectiveSheep! All rights reserved.
+ *
+ *  This file contains Original Code.
+ *  You may not use this file except if authorized by ObjectiveSheep 
+ * -----------------------------------------------------------------
+ *  Created by Fabio Russo on 25/05/2011 
+ *
+ * ****************************************************************/
+
+#import <AppKit/AppKit.h>
+
+@interface OBSLevelIndicatorCell : NSLevelIndicatorCell
+
+@end
